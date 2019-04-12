22da2ec68134a546779cc12b5770a3e61b01fa14
AlfredoJHN <41807769+AlfredoJHN@users.noreply.github.com>
2019-04-12 11:29:50 -0600

    Lista de usuarios y tecnologias, falta flitrar tipo de usuarios

---
+++ SIRERH/Controllers/ListaController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SIRERH.Models
+{
+    public class ListaController : Controller
+    {
+        private Proyecto_P5Entities db = new Proyecto_P5Entities();
+
+
+        // GET: USUARIOs
+        public ActionResult Index()
+        {
+            var usuarios = db.USUARIO.ToList();
+            var tecnologias = db.TECNOLOGIA.ToList();
+            var model = new ListaViewModel()
+            {
+                ListaUsuarios = usuarios,
+                ListaTecnologias = tecnologias
+            };
+
+            return View(model);
+            
+        }
+
+    }
+
+}
+
---
+++ SIRERH/Models/ListaViewModel.cs
@@ -0,0 +1,16 @@
+﻿
+
+
+namespace SIRERH.Models
+{
+    using System;
+    using System.Collections.Generic;
+    public class ListaViewModel
+    {
+        public ListaViewModel()
+        {
+        }
+        public List<USUARIO> ListaUsuarios { get; set; }
+        public List<TECNOLOGIA> ListaTecnologias { get; set; }
+    }
+}
