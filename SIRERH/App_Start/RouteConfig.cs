670fe729d817bf06e06dc6d8318ee8751b92496a
CRjulianTICO <crjuliantico@gmail.com>
2019-04-30 13:15:15 -0600

    Desmadres de Julian

--- SIRERH/App_Start/RouteConfig.cs
+++ SIRERH/App_Start/RouteConfig.cs
@@ -13,11 +13,21 @@ namespace SIRERH
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+
+
+            routes.MapRoute(
+                name: "Index",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Account", action = "Login", id = UrlParameter.Optional }
+            );
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+
         }
     }
 }
--- SIRERH/Controllers/HomeController.cs
+++ SIRERH/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ namespace SIRERH.Controllers
 {
     public class HomeController : Controller
     {
+        [Authorize]
         public ActionResult Index()
         {
             return View();
